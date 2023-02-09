b0d75cb9f71481a9667167f96386f1f4c54c113c
gru13 <guruprasath1302@gmail.com>
2023-02-09 14:06:10 +0530

    merge array
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- merge_arr.c
+++ merge_arr.c
@@ -2,5 +2,28 @@
 
 int main(){
     int len_a,len_b;
-    printf("Enter thr len of Arr 'A','B' : ")
+    //  getting the value for 2 array
+    printf("Enter thr len of Arr 'A','B' : ");
+    scanf("%d %d", &len_a, &len_b);
+    int a[len_a], b[len_b], c[len_a+len_b];
+    
+    //  getting the values for array
+    for(int i = 0; i<len_a; i++){
+        printf("Enter the value for array a[] : ", i);
+        scanf("%d",&a[i]);
+        c[i] = a[i];
+    }
+
+    for(int i = 0; i<len_b; i++){
+        printf("Enter the value for array b[] : ", i);
+        scanf("%d",&b[i]);
+        c[i+len_a] = b[i];
+    }
+    
+    //  printing the final array 
+    puts("The value of combained array :\t");
+    for(int i = 0; i<len_a+len_b; i++){
+        printf("%d\t",c[i]);
+    }
+
 }
--- mul_matr.c
+++ mul_matr.c
@@ -2,16 +2,17 @@
 
 int main(){
     int r1,c1,r2,c2;
+    //  getting inputs for row1, col1 and row2, col2 of matrix A and B;
     puts("Enter r1, c1, r2, c2 : ");
     scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
-
+    //  checking
     if (r2 != c1){
         puts("we can't multiple two matrix");
         return 1;
     }    
 
     int a[r1][c1], b[r2][c2], c[r1][c2];
-
+    // getting value for matrix a and b
     for (int i = 0; i<r1;i++){
         for(int j=0;j<c1;j++){
             printf("Enter the value of mat A [%d][%d] : ", i, j);
@@ -21,11 +22,12 @@ int main(){
 
     for (int i = 0; i < r2; i++){
         for(int j = 0; j < c2; j++){
-            printf("Enter the value of mat A [%d][%d] : ", i, j);
+            printf("Enter the value of mat B [%d][%d] : ", i, j);
             scanf("%d", &b[i][j]);
         }
     }
 
+    // multiplying
 
     for(int i = 0; i<r1; i++){
         for(int j = 0; j<c2; j++){
@@ -36,6 +38,7 @@ int main(){
         }
     }
 
+    // printing the matrix 
     for (int i = 0; i < r1; i++){
         for(int j = 0; j < c2; j++){
             printf("%d\t", c[i][j]);
