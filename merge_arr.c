9d461af6123523491cf7a6a0e5a48b63fa97b8e6
gru13 <guruprasath1302@gmail.com>
2023-02-09 13:52:25 +0530

    1
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

---
+++ merge_arr.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    int len_a,len_b;
+    printf("Enter thr len of Arr 'A','B' : ")
+}
---
+++ mul_matr.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+    int r1,c1,r2,c2;
+    puts("Enter r1, c1, r2, c2 : ");
+    scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
+
+    if (r2 != c1){
+        puts("we can't multiple two matrix");
+        return 1;
+    }    
+
+    int a[r1][c1], b[r2][c2], c[r1][c2];
+
+    for (int i = 0; i<r1;i++){
+        for(int j=0;j<c1;j++){
+            printf("Enter the value of mat A [%d][%d] : ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r2; i++){
+        for(int j = 0; j < c2; j++){
+            printf("Enter the value of mat A [%d][%d] : ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+
+    for(int i = 0; i<r1; i++){
+        for(int j = 0; j<c2; j++){
+            c[i][j] = 0;
+            for(int k = 0; k<r2; k++){
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < r1; i++){
+        for(int j = 0; j < c2; j++){
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
