88acbdbfff549f9a7b6e963eefed004a1a30cabc
gru13 <guruprasath1302@gmail.com>
2023-02-09 14:37:23 +0530

    similarr matrix
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

---
+++ similar_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int r1,c1;
+    //  getting inputs for row1, col1 of matrix A
+    puts("Enter r1, c1,: ");
+    scanf("%d %d", &r1, &c1);
+
+    int a[r1][c1], b[c1][r1];
+
+    // getting value for matrix A
+    for (int i = 0; i < r1; i++){
+        for(int j = 0; j < c1; j++){
+            printf("Enter the value of mat A [%d][%d] : ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < c1; i++){
+        for(int j = 0; j < r1; j++){
+        if(a[i][j] != a[j][i]){
+            puts("this is not similar");
+            return 1;
+        }
+        }
+    }
+    puts("this is similar matrix");
+    return 0;
+}
